343f2ab840d9016af43cc04969d3d4b2fe4e3c4c
sarahwiens <sarahwiens88@gmail.com>
2024-10-09 23:05:56 -0400

    Added API integration to forecast

--- src/script.js
+++ src/script.js
@@ -62,7 +62,7 @@ function updateCurrentTemperature(response) {
   )}`;
 }
 
-function updateWeatherElements(response) {
+function updateCurrentWeatherElements(response) {
   updateCurrentCity(response);
   updateCurrentDay(response);
   updateCurrentTime(response);
@@ -73,64 +73,70 @@ function updateWeatherElements(response) {
   updateCurrentTemperature(response);
 }
 
-function getCurrentWeatherDetails(apiURL) {
-  axios.get(apiURL).then(updateWeatherElements);
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
 }
 
-function getForecastWeatherDetails(forecastApiURL) {
-  axios.get(forecastApiURL).then(displayForecast);
-}
+function displayForecast(response) {
+  console.log(response.data);
 
-function determineApiUrL(city) {
-  let apiKey = "7f314bd46t448eb65o54002ab9dadc03";
-  let units = "metric";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
-  getCurrentWeatherDetails(apiURL);
-}
-
-function determineForecastApiUrl(city) {
-  let apiKey = "7f314bd46t448eb65o54002ab9dadc03";
-  let units = "metric";
-  let forecastApiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}&units=${units}`;
-  getForcastWeatherDetails(forecastApiURL);
-}
-
-function handleSearchCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-city-form-input");
-  let city = searchInput.value.toLowerCase().trim();
-  determineApiUrL(city);
-  determineForecastApiUrl(city);
-}
-
-function displayForecast() {
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="forecast-weather-day-container">
-        <div class="forecast-weather-day">${day}</div>
-        <div class="forecast-weather-icon">☀</div>
+        <div class="forecast-weather-day">${formatForecastDay(day.time)}</div>
+        <div ><img src= "${
+          day.condition.icon_url
+        }" class="forecast-weather-icon"/></div>
         <div class="forecast-weather-temperature">
           <div class="forecast-weather-temperature-high">
-            <strong>26°C</strong>
+            <strong>${Math.round(day.temperature.maximum)}°C</strong>
           </div>
-          <div class="forecast-weather-temperature-low">18°C</div>
+          <div class="forecast-weather-temperature-low">${Math.round(
+            day.temperature.minimum
+          )}°C</div>
         </div>
       </div>
       `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast-weather-container");
   forecastElement.innerHTML = forecastHtml;
 }
 
+function getCurrentWeatherDetails(currentApiUrl) {
+  axios.get(currentApiUrl).then(updateCurrentWeatherElements);
+}
+
+function getForecastWeatherDetails(forecastApiUrl) {
+  axios.get(forecastApiUrl).then(displayForecast);
+}
+
+function determineApiUrls(city) {
+  let apiKey = "7f314bd46t448eb65o54002ab9dadc03";
+  let units = "metric";
+  let currentApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  getCurrentWeatherDetails(currentApiUrl);
+  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+  getForecastWeatherDetails(forecastApiUrl);
+}
+
+function handleSearchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-city-form-input");
+  let city = searchInput.value.toLowerCase().trim();
+  determineApiUrls(city);
+}
+
 let now = new Date();
 let searchCityFormElement = document.querySelector("#search-city-form");
 searchCityFormElement.addEventListener("submit", handleSearchCity);
 
-determineApiUrL("Paris");
-displayForecast();
+determineApiUrls("Paris");
